15d858cee76729f944bc60cc93f205bf645e0eaf
zm2666 <83871614+zm2666@users.noreply.github.com>
2023-03-02 19:34:39 +0100

    Refactoring tasks controller and tasks router

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,23 +1,21 @@
 import { Task } from './tasks.entity';
 import { AppDataSource } from '../../index';
 import { instanceToPlain } from 'class-transformer';
+import { Request, Response } from 'express';
 
-export class TaskController {
-    constructor(
-        private taskRepository = AppDataSource.getRepository(
-            Task,
-        ),
-    ) {}
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    public async getAll(): Promise<Task[]> {
+class TasksController {
+    public async getAll(
+        req: Request,
+        res: Response,
+    ): Promise<Response> {
         //Declare a variable to hold all tasks
         let allTasks: Task[];
 
         //Fetch all tasks using the repository
         try {
-            allTasks = await this.taskRepository.find({
+            allTasks = await AppDataSource.getRepository(
+                Task,
+            ).find({
                 order: {
                     date: 'ASC',
                 },
@@ -25,9 +23,13 @@ export class TaskController {
             //Convert the tasks instance to an array of objects
             allTasks = instanceToPlain(allTasks) as Task[];
 
-            return allTasks;
-        } catch (errors) {
-            console.log(errors);
+            return res.json(allTasks).status(200);
+        } catch (_errors) {
+            return res
+                .json({ erros: 'Internal Server Error' })
+                .status(500);
         }
     }
 }
+
+export const tasksController = new TasksController();
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from 'express';
-import { TaskController } from './tasks.controller';
+import { tasksController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
 import { validationResult } from 'express-validator/src/validation-result';
 
@@ -7,14 +7,7 @@ import { validationResult } from 'express-validator/src/validation-result';
 export const tasksRouter: Router = Router();
 
 //Create a default route
-tasksRouter.get(
-    '/tasks',
-    async (req: Request, res: Response) => {
-        const taskController = new TaskController();
-        const allTasks = await taskController.getAll();
-        res.json(allTasks).status(200);
-    },
-);
+tasksRouter.get('/tasks', tasksController.getAll);
 
 tasksRouter.post(
     '/tasks',
