40bb0c5379a6c7519d07124f6f37dacccfb001e8
zm2666 <83871614+zm2666@users.noreply.github.com>
2023-03-02 17:10:29 +0100

    Added cors, bodyParser and types/cors, connected to database

--- index.ts
+++ index.ts
@@ -1,17 +1,51 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, {
+    Express,
+    Request,
+    Response,
+} from 'express';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 //Instantiate express app
 const app: Express = express();
 dotenv.config();
 
+//Parse request.Body
+app.use(bodyParser.json());
+
+//Use CORS install types as well
+app.use(cors());
+
+//Create Database Connection
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    synchronize: true,
+});
+
 //Define server port
 const port = process.env.PORT;
 
 //Create a default route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
 });
 
-//Start listening to the requests on the defined port
-app.listen(port);
+AppDataSource.initialize()
+    .then(() => {
+        //Start listening to the requests on the defined port
+        app.listen(port);
+        console.log('Data Source has been initialized');
+    })
+    .catch((err) => {
+        console.error(
+            'Error during data source initialization',
+            err,
+        );
+    });
